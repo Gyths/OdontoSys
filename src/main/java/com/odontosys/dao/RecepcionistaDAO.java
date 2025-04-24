443ab91f8074d811ad5dc2e565ec51d639e5c46d
Gyths <123425129+Gyths@users.noreply.github.com>
2025-04-23 21:48:56 -0500

    Update RecepcionistaDAO.java

--- src/main/java/com/odontosys/dao/RecepcionistaDAO.java
+++ src/main/java/com/odontosys/dao/RecepcionistaDAO.java
@@ -3,7 +3,7 @@ package com.odontosys.dao;
 import com.odontosys.users.model.Recepcionista;
 import java.util.ArrayList;
 
-public interface RecepcionistaDAO {
+public interface RecepcionistaDAO{
     Integer insertar(Recepcionista recepcionista);
     Integer modificarCorreo(int idUsuario, String nuevoCorreo);
     Integer eliminar(int idUsuario);
