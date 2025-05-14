53759d0a3b3801a9aca93bb22c35e4bcbd4dfda5
Gyths <123425129+Gyths@users.noreply.github.com>
2025-05-13 22:05:55 -0500

    Insert Del BO
    Sala tratamiento y turno creados

--- OdontoSysV2/OdontoSys/OdontoSysBussiness/src/main/java/com/odontosys/bo/TratamientoBO.java
+++ OdontoSysV2/OdontoSys/OdontoSysBussiness/src/main/java/com/odontosys/bo/TratamientoBO.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.odontosys.bo;
 
-/**
- *
- * @author Gyts
- */
+import com.odontosys.dao.TratamientoDAO;
+import com.odontosys.daoImp.TratamientoDAOImpl;
+import com.odontosys.services.model.Especialidad;
+import com.odontosys.services.model.Tratamiento;
+import java.util.ArrayList;
+
+
 public class TratamientoBO {
+    private TratamientoDAO tratamientoDAO;
     
+    public TratamientoBO(){
+        this.tratamientoDAO = new TratamientoDAOImpl();
+    }
+    
+    public Integer InsertarTratamiento(String nombre, String descripcion, double costo, Especialidad especialidad){
+        Tratamiento tratamiento = new Tratamiento();
+        tratamiento.setNombre(nombre);
+        tratamiento.setDescripcion(descripcion);
+        tratamiento.setCosto(costo);
+        tratamiento.setEspecialidad(especialidad);
+        
+        return this.tratamientoDAO.insertar(tratamiento);
+    }
+    
+    //Pendiente
+    public Integer ModificarTratamiento(String nombre){
+        return -1;
+    }
+    
+    public Integer EliminarTratamiento(String nombre){
+        ArrayList<Tratamiento> list = this.tratamientoDAO.listarTodos();
+        for(Tratamiento t : list){
+            if(t.getNombre().matches(nombre))
+                return this.tratamientoDAO.eliminar(t);
+        }
+        return -1;
+    }
 }
