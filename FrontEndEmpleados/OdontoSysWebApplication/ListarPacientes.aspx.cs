edb8bc28a4ee0cca2ee52aaaff33a1796e7564e9
Farid <a20210962@pucp.edu.pe>
2025-06-17 10:51:55 -0500

    a

--- FrontEndEmpleados/OdontoSysWebApplication/ListarPacientes.aspx.cs
+++ FrontEndEmpleados/OdontoSysWebApplication/ListarPacientes.aspx.cs
@@ -1,17 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+using OdontoSysWebApplication.OdontoSysBusiness; 
 
 namespace OdontoSysWebApplication
 {
     public partial class ListarPacientes : System.Web.UI.Page
     {
+        
+        private PacienteBO boPaciente;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+            if (!IsPostBack)
+            {
+                CargarPacientes();
+            }
+        }
 
+        private void CargarPacientes()
+        {
+            try
+            {
+      
+                boPaciente = new PacienteBO();
+                var listaPacientes = boPaciente.paciente_listarTodos();
+                gvPacientes.DataSource = listaPacientes;
+                gvPacientes.DataBind();
+            }
+            catch (Exception ex)
+            {
+              
+                System.Diagnostics.Debug.WriteLine("Error al cargar pacientes: " + ex.Message);
+                gvPacientes.DataSource = null;
+                gvPacientes.DataBind();
+            }
         }
     }
 }
