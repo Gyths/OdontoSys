363ac34f857f2d29dc36cfe07b7197660f0a0946
Farid <a20210962@pucp.edu.pe>
2025-06-18 00:36:23 -0500

    a

---
+++ FrontEndEmpleados/OdontoSysWebApplication/VerComprobante.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using OdontoSysWebAppliation.OdontoSysBusiness;
+using OdontoSysWebApplication.OdontoSysBusiness;
+
+namespace OdontoSysWebApplication
+{
+    public partial class VerComprobante : System.Web.UI.Page
+    {
+        private ComprobanteBO boComprobante;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string idComprobanteStr = Request.QueryString["idComprobante"];
+
+                if (!string.IsNullOrEmpty(idComprobanteStr) && int.TryParse(idComprobanteStr, out int idComprobante))
+                {
+                    CargarComprobante(idComprobante);
+                }
+                else
+                {
+                    MostrarError();
+                }
+            }
+        }
+
+        private void CargarComprobante(int idComprobante)
+        {
+            try
+            {
+                boComprobante = new ComprobanteBO();
+                var comprobante = boComprobante.comprobante_obtenerPorId(idComprobante);
+
+                if (comprobante != null)
+                {
+                    MostrarDatosComprobante(comprobante);
+                }
+                else
+                {
+                    MostrarError();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log del error 
+                MostrarError();
+            }
+        }
+
+        private void MostrarDatosComprobante(dynamic comprobante)
+        {
+            try
+            {
+              
+                txtIdComprobante.Text = comprobante.idComprobante?.ToString() ?? "";
+
+                
+                if (comprobante.fecha != null)
+                {
+                    if (DateTime.TryParse(comprobante.fecha.ToString(), out DateTime fecha))
+                    {
+                        txtFecha.Text = fecha.ToString("dd/MM/yyyy");
+                    }
+                    else
+                    {
+                        txtFecha.Text = comprobante.fecha.ToString();
+                    }
+                }
+
+                // Formatear monto total
+                if (comprobante.montoTotal != null)
+                {
+                    if (decimal.TryParse(comprobante.montoTotal.ToString(), out decimal monto))
+                    {
+                        txtMontoTotal.Text = monto.ToString("S");
+                    }
+                    else
+                    {
+                        txtMontoTotal.Text = comprobante.montoTotal.ToString();
+                    }
+                }
+
+               
+                txtTipoComprobante.Text = comprobante.tipoComprobante?.ToString() ?? "";
+                txtEstado.Text = comprobante.estado?.ToString() ?? "";
+                txtMetodoPago.Text = comprobante.metodoPago?.ToString() ?? "";
+                txtObservaciones.Text = comprobante.observaciones?.ToString() ?? "";
+
+               
+            }
+            catch (Exception ex)
+            {
+                MostrarError();
+            }
+        }
+
+        private void MostrarError()
+        {
+            pnlError.Visible = true;
+        }
+    }
+}
---
+++ FrontEndEmpleados/OdontoSysWebApplication/VerComprobante.aspx.designer.cs
@@ -0,0 +1,89 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace OdontoSysWebApplication
+{
+
+
+    public partial class VerComprobante
+    {
+
+        /// <summary>
+        /// Control txtIdComprobante.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtIdComprobante;
+
+        /// <summary>
+        /// Control txtFecha.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFecha;
+
+        /// <summary>
+        /// Control txtMontoTotal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtMontoTotal;
+
+        /// <summary>
+        /// Control txtTipoComprobante.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTipoComprobante;
+
+        /// <summary>
+        /// Control txtEstado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEstado;
+
+        /// <summary>
+        /// Control txtMetodoPago.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtMetodoPago;
+
+        /// <summary>
+        /// Control txtObservaciones.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtObservaciones;
+
+        /// <summary>
+        /// Control pnlError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlError;
+    }
+}
